4064b80d2c4c328c3386e9836d02f637895276f7
FREDYK1 <fkwekukankam@gmail.com>
2024-11-17 01:01:53 +0000

    Divide html code into pages and create the about page

--- job_application/views.py
+++ job_application/views.py
@@ -36,3 +36,6 @@ def home(request):
             messages.success(request, "Application submitted successfully")
 
     return render(request,"home.html")
+
+def about(request):
+    return render(request,"about.html")
